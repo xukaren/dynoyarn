45d37ee6f571bb8c80e9440f0321145e1657b35c
Karen Xu <karenx@uber.com>
2022-10-12 20:13:35 -0400

    increase NM memory to 120g

--- dynoyarn-generator/src/main/java/com/linkedin/dynoyarn/workload/simulation/SimulatedApplicationMaster.java
+++ dynoyarn-generator/src/main/java/com/linkedin/dynoyarn/workload/simulation/SimulatedApplicationMaster.java
@@ -126,16 +126,16 @@ public class SimulatedApplicationMaster {
     // TODO hardcoded due to fs.open() trying to access local FS instead of HDFS
 
     // Bronze 1T
-    rmEndpoint = "phx3-xip.prod.uber.internal:8032";
-    amEndpoint = "phx3-xip.prod.uber.internal:8030";
+    // rmEndpoint = "phx3-xip.prod.uber.internal:8032";
+    // amEndpoint = "phx3-xip.prod.uber.internal:8030";
 
     // Bronze B20B
     // rmEndpoint = "phx5-69a.prod.uber.internal:8032";
     // amEndpoint = "phx5-69a.prod.uber.internal:8030";
 
     // Gamma 1T baremetal 
-    // rmEndpoint = "hadoopgammarm01-dca8.prod.uber.internal:8032"; 
-    // amEndpoint = "hadoopgammarm01-dca8.prod.uber.internal:8030";
+    rmEndpoint = "hadoopgammarm01-dca8.prod.uber.internal:8032"; 
+    amEndpoint = "hadoopgammarm01-dca8.prod.uber.internal:8030";
     
     // gamma b20b containerized RM 
     // rmEndpoint = "dca18-5k2.prod.uber.internal:8032"; 
